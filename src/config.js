72684f37cccd185077f3cb9ea1b8a6b153e71374
Nicole Fafard <nicole.fafard@gmail.com>
2019-12-12 12:16:42 -0700

    db fix

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
-  DB_URL:
+  DATABASE_URL:
     process.env.DATABASE_URL ||
     "postgresql://dunder_mifflin:1234@localhost/i-ate",
   TEST_DB_URL:
